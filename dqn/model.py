21356a1d5f701c6f7dc9766fffad5acf8d999f03
Hee Jung Ryu <imiheej@gmail.com>
2019-04-12 00:19:15 -0700

    A solution for OpenAI Gym LunarLander-v2 using Deep Q-Network.

---
+++ dqn/model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_dim=64, fc2_dim=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_dim)
+        self.fc2 = nn.Linear(fc1_dim, fc2_dim)
+        self.out = nn.Linear(fc2_dim, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.out(x)
